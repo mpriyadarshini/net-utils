05288380029adc13599b49520efe9e1be8a08692
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-04-17 15:13:49 +0530

    HttpCat

---
+++ HttpCat.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.*;
+import java.net.*;
+
+class HttpCat{
+    public static void main(String[] args) throws IOException  {
+        URL url = new URL(args[0]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        int c;
+        while((c=br.read())!=-1){
+            System.out.println((char)c);
+        }
+        
+
+    }
+}
